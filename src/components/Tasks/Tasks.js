368071d6f31189077e3340f551115d024c8be165
MohammadMahdiSalmani <msas.mohammad1382@yahoo.com>
2022-08-23 03:28:30 +0430

    Develope Tasks.js with functional component AND Add Redux package

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Task from "../Task/Task";
 import Button from "../Button/Button";
 import Check from "../Check/Check";
@@ -9,29 +9,20 @@ import TaskContent from "../TaskContent/TaskContent";
 import "./Tasks.css"
 import axios from "../../axios";
 
-class Tasks extends React.Component {
-    state = {
-        allTasks: [],
-        newValue: {
-            id: null,
-            newValue: ""
-        },
-        disableButton: false
-    }
+const Tasks = () => {
+    const [allTasks, setAllTasks] = useState([])
+    const [newValue, setNewValue] = useState({ id: null, newValue: "" })
 
-    componentDidMount() {
-        axios.get("/tasks.json").then((response) => {
-            this.setState({ allTasks: response.data })
-        })
-    }
+    useEffect(() => {
+        const sendRequest = async () => {
+            const response = await axios.get("/tasks.json")
+            setAllTasks(response.data)
+        }
 
-    componentDidUpdate() {
-        axios.get("/tasks.json").then((response) => {
-            this.setState({ allTasks: response.data })
-        })
-    }
+        sendRequest()
+    }, [allTasks, newValue])
 
-    deleteHandler = (id) => {
+    const deleteHandler = (id) => {
         axios.delete(`/tasks/${id}.json`)
             .then(response => {
                 console.log(response)
@@ -41,7 +32,7 @@ class Tasks extends React.Component {
             })
     }
 
-    editHandler = (id) => {
+    const editHandler = (id) => {
         axios.put(`/tasks/${id}/readOnly.json`, "false")
             .then(response => {
                 console.log(response)
@@ -51,7 +42,7 @@ class Tasks extends React.Component {
             })
     }
 
-    enterHandler = (id) => {
+    const enterHandler = (id) => {
         axios.put(`/tasks/${id}/readOnly.json`, "true")
             .then(response => {
                 console.log(response)
@@ -61,10 +52,9 @@ class Tasks extends React.Component {
             })
 
 
-        const preValue = { ...this.state.newValue[id] }
-        const newValue = JSON.stringify(preValue.newValue)
-        const clearedValue = newValue.replace(/  +/g, ' ')
-        console.log(clearedValue)
+        const preValue = { ...newValue[id] }
+        const newVal = JSON.stringify(preValue.newValue)
+        const clearedValue = newVal.replace(/  +/g, ' ')
 
         if (clearedValue) {
             axios.put(`/tasks/${id}/task.json`, clearedValue)
@@ -77,10 +67,10 @@ class Tasks extends React.Component {
         }
     }
 
-    newValueHandler = (event, id) => {
+    const newValueHandler = (event, id) => {
         if (event.currentTarget.textContent.length > 0 && event.currentTarget.textContent.length < 170) {
             const updatedValue = {
-                ...this.state.newValue,
+                ...newValue,
             }
 
             const updatedElement = { ...updatedValue[id] }
@@ -89,12 +79,12 @@ class Tasks extends React.Component {
 
             updatedValue[id] = updatedElement
 
-            this.setState({ newValue: updatedValue })
+            setNewValue(updatedValue)
         }
     }
 
-    checkHandler = (id) => {
-        if (this.state.allTasks[id].check) {
+    const checkHandler = (id) => {
+        if (allTasks[id].check) {
             axios.put(`/tasks/${id}/check.json`, "false")
                 .then(response => {
                     console.log(response)
@@ -114,15 +104,14 @@ class Tasks extends React.Component {
         }
     }
 
-    render() {
-        const tasks = []
+    const tasks = []
 
-        for (let item in this.state.allTasks) {
-            tasks.push({
-                id: item,
-                value: this.state.allTasks[item],
-            })
-        }
+    for (let item in allTasks) {
+        tasks.push({
+            id: item,
+            value: allTasks[item],
+        })
+
 
         return (
             <section>
@@ -130,12 +119,12 @@ class Tasks extends React.Component {
                     {tasks.map((item) => {
                         return (
                             <Task key={item.id} style={{ borderBottom: item.value.readOnly ? "none" : "3px solid #ffbc1e", borderRadius: item.value.readOnly ? "10px" : "10px 10px 0 0" }}>
-                                <TaskContent className="task-content" style={{ textDecoration: item.value.check ? 'line-through' : 'none' }} change={(event) => this.newValueHandler(event, item.id)} contentEditable={!item.value.readOnly}>{item.value.task}</TaskContent>
-                                <Check className="check" change={() => this.checkHandler(item.id)} checked={item.value.check} />
+                                <TaskContent className="task-content" style={{ textDecoration: item.value.check ? 'line-through' : 'none' }} change={(event) => newValueHandler(event, item.id)} contentEditable={!item.value.readOnly}>{item.value.task}</TaskContent>
+                                <Check className="check" change={() => checkHandler(item.id)} checked={item.value.check} />
                                 <div className="controllers">
-                                    <Button btnColor="btn-info" controller={() => this.editHandler(item.id)} style={{ display: item.value.readOnly ? 'block' : 'none' }}>Edit</Button>
-                                    <Button btnColor="btn-danger" controller={() => this.deleteHandler(item.id)} style={{ display: item.value.readOnly ? 'block' : 'none' }}>Delete</Button>
-                                    <Button btnColor="btn-success" controller={() => this.enterHandler(item.id)} style={{ display: item.value.readOnly ? 'none' : 'block' }} disabled={this.state.disableButton}>Enter</Button>
+                                    <Button btnColor="btn-info" controller={() => editHandler(item.id)} style={{ display: item.value.readOnly ? 'block' : 'none' }}>Edit</Button>
+                                    <Button btnColor="btn-danger" controller={() => deleteHandler(item.id)} style={{ display: item.value.readOnly ? 'block' : 'none' }}>Delete</Button>
+                                    <Button btnColor="btn-success" controller={() => enterHandler(item.id)} style={{ display: item.value.readOnly ? 'none' : 'block' }}>Enter</Button>
                                 </div>
                             </Task>
                         )
